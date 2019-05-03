47bce2eeb98e01f907b5744a2a38e82bfb75ce67
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2019-05-02 22:31:56 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once "includes/header.inc.php";
+    include_once "navbar.php";
+?>
+
+<?php
+    include_once "includes/footer.inc.php";
+?>
---
+++ navbar.php
@@ -0,0 +1,41 @@
+<header class="container--header">
+    <i class="hamburger-header fas fa-bars"></i>
+    <div class="logo">LOGO</div>
+
+    <nav class="navbar">
+        <ul class="list--header navbar--list">
+            <li><a href="#" class="link--header">About</a></li>
+            <li><a href="#" class="link--header">Users</a></li>
+            <li><a href="#" class="link--header">Submissions</a></li>
+            <li><a href="#" class="link--header">FAQ</a></li>
+            <form class="searchform"action="" method="get">
+                <input class="searchbar--header" type="text" name="search" placeholder="Search...">
+                <input class="btn-hidden" type="submit">
+            </form>
+        </ul>
+    </nav>
+
+    <ul class="list--header signin--list">
+        <li><a href="#" class="link--header">Sign up</a></li>
+        <li><a href="#" class="link--header">Sign in</a></li>
+    </ul>
+</header>
+
+<section class="mobile-navmenu">
+    <form action="" method="get">
+        <input class="searchbar--header" type="text" name="search" placeholder="Search...">
+        <input class="btn-hidden" type="submit">
+    </form>
+    <nav class="mobile-navbar">
+        <ul class="list--header list--navbar">
+            <li><a href="#" class="link--header">About</a></li>
+            <li><a href="#" class="link--header">Users</a></li>
+            <li><a href="#" class="link--header">Submissions</a></li>
+            <li><a href="#" class="link--header">FAQ</a></li>
+        </ul>
+    </nav>
+    <ul class="list--header signin--list">
+        <li><a href="#" class="link--header">Sign up</a></li>
+        <li><a href="#" class="link--header">Sign in</a></li>
+    </ul>
+</section>
