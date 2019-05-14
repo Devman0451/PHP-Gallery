3863f314cff410238a07ff1dfbdab4abfe36ecc8
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2019-05-14 11:38:37 -0400

    added search bar functionality

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ include_once "navbar.php";
     ?>
     <section class="imagelinks--container">
         <ul class="imagelinks--list">
-            <li><a href="#" rel="">Lastest</a></li>
-            <li><a href="#" rel="">Trending</a></li>
+            <li><a href="index.php?load=latest" rel="">Lastest</a></li>
+            <li><a href="index.php?load=trending" rel="">Trending</a></li>
         </ul>
     </section>
 
@@ -34,259 +34,104 @@ include_once "navbar.php";
 
         <?php
 
-        //Load Images from db
+
         include_once 'config/db.php';
 
-        $sql = "SELECT id, title, uploader, image_thumb FROM images ORDER BY date DESC LIMIT 30;";
-        $stmt = mysqli_stmt_init($conn);
+        //Load images from database based on search fields
+        if (isset($_GET['search'])) {
 
-        if (mysqli_stmt_prepare($stmt, $sql)) {
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt, $id, $title, $uploader, $image_thumb);
+            $search = $_GET['search'];
 
-            while (mysqli_stmt_fetch($stmt)) {
-                echo '<a href="post.php?img=' . $id . '" class="image-link">
-                        <div class="project">
-                            <div class="overlay">
-                                <div class="info">
-                                    <img src="uploads/icons/default.jpg" alt="profile" class="avatar">
-                                    <div class="image-info">
-                                        <div class="title">' . $title . '</div>
-                                        <div class="author">' . $uploader . '</div>
+            //Searches tags and title for keywords
+            $sql = "SELECT id, title, uploader, image_thumb FROM images WHERE tags LIKE CONCAT('%', ?, '%') OR title LIKE CONCAT('%', ?, '%');";
+            $stmt = mysqli_stmt_init($conn);
+
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_bind_param($stmt, 'ss', $search, $search);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                mysqli_stmt_bind_result($stmt, $id, $title, $uploader, $image_thumb);
+
+                if (mysqli_stmt_num_rows($stmt) == 0) {
+                    echo '<p class="search-msg">No Results Found</p>';
+                } else {
+                    while (mysqli_stmt_fetch($stmt)) {
+                        echo '<a href="post.php?img=' . $id . '" class="image-link">
+                                <div class="project">
+                                    <div class="overlay">
+                                        <div class="info">
+                                            <img src="uploads/icons/default.jpg" alt="profile" class="avatar">
+                                            <div class="image-info">
+                                                <div class="title">' . $title . '</div>
+                                                <div class="author">' . $uploader . '</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="' . $image_thumb . '" alt="" class="image">
+                                </div>
+                             </a>';
+                    }
+                }
+            } else {
+                echo '<p>ERROR</p>';
+            }
+        } else if (isset($_GET['load']) && $_GET['load'] == 'trending') {
+            
+            //Load the trending images
+            $sql = "SELECT id, title, uploader, image_thumb FROM images ORDER BY date DESC LIMIT 30;";
+            $stmt = mysqli_stmt_init($conn);
+    
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt, $id, $title, $uploader, $image_thumb);
+    
+                while (mysqli_stmt_fetch($stmt)) {
+                    echo '<a href="post.php?img=' . $id . '" class="image-link">
+                            <div class="project">
+                                <div class="overlay">
+                                    <div class="info">
+                                        <img src="uploads/icons/default.jpg" alt="profile" class="avatar">
+                                        <div class="image-info">
+                                            <div class="title">' . $title . '</div>
+                                            <div class="author">' . $uploader . '</div>
+                                        </div>
                                     </div>
                                 </div>
+                                <img src="' . $image_thumb . '" alt="" class="image">
                             </div>
-                            <img src="' . $image_thumb . '" alt="" class="image">
-                        </div>
-                     </a>';
+                         </a>';
+                }
             }
-        }
+        } else if (isset($_GET['load']) && $_GET['load'] == 'latest') {
+            
+            //Load the latest images
+            $sql = "SELECT id, title, uploader, image_thumb FROM images ORDER BY date DESC LIMIT 30;";
+            $stmt = mysqli_stmt_init($conn);
+    
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt, $id, $title, $uploader, $image_thumb);
+    
+                while (mysqli_stmt_fetch($stmt)) {
+                    echo '<a href="post.php?img=' . $id . '" class="image-link">
+                            <div class="project">
+                                <div class="overlay">
+                                    <div class="info">
+                                        <img src="uploads/icons/default.jpg" alt="profile" class="avatar">
+                                        <div class="image-info">
+                                            <div class="title">' . $title . '</div>
+                                            <div class="author">' . $uploader . '</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="' . $image_thumb . '" alt="" class="image">
+                            </div>
+                         </a>';
+                }
+            }
+        } 
+
         ?>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icons/default.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
-
-        <a href="" class="image-link">
-            <div class="project">
-                <div class="overlay">
-                    <div class="info">
-                        <img src="uploads/icon2.jpg" alt="profile" class="avatar">
-                        <div class="image-info">
-                            <div class="title">Title</div>
-                            <div class="author">Author</div>
-                        </div>
-                    </div>
-                </div>
-                <img src="uploads/img1.jpg" alt="" class="image">
-            </div>
-        </a>
 
     </section>
 </main>
